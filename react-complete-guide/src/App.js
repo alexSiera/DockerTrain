6eb5b7e6ec820fd4ad0dd9c76b2aa9b9a014c455
sieraAlex <fa1nt.siera@gmail.com>
2019-10-23 20:30:18 +0300

    add map method to render a list

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -84,7 +84,12 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          <Person
+          {this.state.persons.map(person => {
+            return (
+              <Person name={person.name} age={person.age} key={person.name} />
+            );
+          })}
+          {/* <Person
             name={this.state.persons[0].name}
             age={this.state.persons[0].age}
           />
@@ -99,7 +104,7 @@ class App extends Component {
           <Person
             name={this.state.persons[2].name}
             age={this.state.persons[2].age}
-          />
+          /> */}
         </div>
       );
     }
