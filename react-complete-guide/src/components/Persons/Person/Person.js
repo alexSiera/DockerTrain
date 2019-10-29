ae475970deec438e23fe8584dcf864dd7d5f23e3
sieraAlex <fa1nt.siera@gmail.com>
2019-10-29 11:29:13 +0300

    add array to the return statement

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -3,14 +3,12 @@ import classes from "./Person.css";
 const person = ({ name, age, children, click, changed }) => {
   console.log("[Person.js] rendering!!!, ");
 
-  return (
-    <div className={classes.Person}>
-      <p onClick={click}>
-        I'am {name} i am {age}
-      </p>
-      <p>{children}</p>
-      <input type="text" onChange={changed} value={name} />
-    </div>
-  );
+  return [
+    <p key="i1" onClick={click}>
+      I'am {name} i am {age}
+    </p>,
+    <p key="i2">{children}</p>,
+    <input key="i3" type="text" onChange={changed} value={name} />
+  ];
 };
 export default person;
