28793fc85a45f8eb0be09fc5dbccda6cdac4a11a
sieraAlex <fa1nt.siera@gmail.com>
2019-10-29 10:01:20 +0300

    add Pure component and optimize shouldComponentUpdate

--- react-complete-guide/src/components/Cockpit/Cockpit.js
+++ react-complete-guide/src/components/Cockpit/Cockpit.js
@@ -25,10 +25,10 @@ const cockpit = props => {
   if (props.showPerson) {
     btnClass = classes.Red;
   }
-  if (props.persons.length <= 2) {
+  if (props.personsLength <= 2) {
     assignedClasses.push(classes.red);
   }
-  if (props.persons.length <= 1) {
+  if (props.personsLength <= 1) {
     assignedClasses.push(classes.bold);
   }
   return (
@@ -41,4 +41,4 @@ const cockpit = props => {
     </React.Fragment>
   );
 };
-export default cockpit;
+export default React.memo(cockpit);
