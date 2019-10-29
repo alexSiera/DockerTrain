a2862f26d240d7c8ba0dc4a784446c3e862cb8c2
sieraAlex <fa1nt.siera@gmail.com>
2019-10-29 11:40:52 +0300

    add Aux component to wrap jsx code

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -1,14 +1,17 @@
 import React from "react";
 import classes from "./Person.css";
+import Aux from "../../../hoc/Aux";
 const person = ({ name, age, children, click, changed }) => {
   console.log("[Person.js] rendering!!!, ");
 
-  return [
-    <p key="i1" onClick={click}>
-      I'am {name} i am {age}
-    </p>,
-    <p key="i2">{children}</p>,
-    <input key="i3" type="text" onChange={changed} value={name} />
-  ];
+  return (
+    <Aux>
+      <p onClick={click}>
+        I'am {name} i am {age}
+      </p>
+      <p>{children}</p>
+      <input type="text" onChange={changed} value={name} />
+    </Aux>
+  );
 };
 export default person;
